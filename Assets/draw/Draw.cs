271f70ae8e2cb35615f35495d7deb464d81405a8
Ilucik <62407205+Ilucik@users.noreply.github.com>
2021-10-20 14:38:03 +0500

    Прототип рисования
    Добавил в проект SteamVr, прототип рисования

---
+++ Assets/draw/Draw.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+    List<Vector3> linePoints;
+    float timer;
+    public float timerDelay;
+
+    public GameObject brush;
+    LineRenderer drawLine;
+    void Start()
+    {
+        linePoints = new List<Vector3>();
+        timer = timerDelay;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "wall")
+        {
+            GameObject newLine = Instantiate(brush);
+            drawLine = newLine.GetComponent<LineRenderer>();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "wall")
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                linePoints.Add(transform.position);
+                drawLine.positionCount = linePoints.Count;
+                drawLine.SetPositions(linePoints.ToArray());
+
+                timer = timerDelay;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "wall")
+        {
+            linePoints.Clear();
+        }
+    }
+}
