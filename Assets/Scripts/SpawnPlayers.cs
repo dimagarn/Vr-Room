7a76acd022f516f2d7bce8646d237677a7713676
JunkerSoft <chebupelmen20@gmail.com>
2021-10-24 20:31:06 +0500

    try

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -6,9 +6,16 @@ using Photon.Pun;
 public class SpawnPlayers : MonoBehaviour
 {
     public GameObject player;
-    
+    public GameObject pen;
+    private static bool firstStart;
+
     void Start()
     {
         PhotonNetwork.Instantiate(player.name, new Vector3(), Quaternion.identity);
+        if (firstStart)
+        {
+            PhotonNetwork.Instantiate(pen.name, new Vector3(-0.279f,1.085521f, 1.905365f), Quaternion.identity);
+            firstStart = false;
+        }
     }
 }
