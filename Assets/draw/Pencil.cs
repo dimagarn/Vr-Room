6992de9285f57d9778541a8570eb799e837c2609
JunkerSoft <chebupelmen20@gmail.com>
2021-12-29 21:04:20 +0500

    не получилось

--- Assets/draw/Pencil.cs
+++ Assets/draw/Pencil.cs
@@ -10,6 +10,7 @@ public class Pencil : MonoBehaviour
     public UnityEvent DestroyPen;
     private bool isUIreleased = false;
     private bool isNotpressed = true;
+    public GameObject pencil;
     private void HandAttachedUpdate(Hand hand)
     {
         GrabTypes bestGrab = hand.GetBestGrabbingType(GrabTypes.Grip, true);
@@ -32,10 +33,10 @@ public class Pencil : MonoBehaviour
 
     public void ChangeTool(GameObject tool)
     {
-        var oldTool = gameObject.transform.GetChild(1).gameObject;
+        var oldTool = pencil.transform.GetChild(1).gameObject;
         Destroy(oldTool, .1f);
         var newTool = Instantiate(tool);
-        newTool.transform.parent = transform;
+        newTool.transform.parent = pencil.transform;
         newTool.transform.localPosition =  new Vector3(0.05259991f, 0, 0.22f);
         newTool.transform.localScale = new Vector3(1, 1, 1);        
     }
