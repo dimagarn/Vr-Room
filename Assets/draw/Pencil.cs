19ddf8dd4a243ff15e522cb4318f04a386519de5
Ilucik <62407205+Ilucik@users.noreply.github.com>
2021-12-29 23:01:03 +0500

    фикс конфликтов

--- Assets/draw/Pencil.cs
+++ Assets/draw/Pencil.cs
@@ -38,15 +38,8 @@ public class Pencil : MonoBehaviour
 
     public void ChangeTool(GameObject tool)
     {
-<<<<<<< Updated upstream
+
         var oldTool = pencil.transform.GetChild(1).gameObject;
-=======
-<<<<<<< HEAD
-        var oldTool = pencil.gameObject.transform.GetChild(1).gameObject;
-=======
-        var oldTool = pencil.transform.GetChild(1).gameObject;
->>>>>>> 6992de9285f57d9778541a8570eb799e837c2609
->>>>>>> Stashed changes
         Destroy(oldTool, .1f);
         var newTool = Instantiate(tool);
         newTool.transform.parent = pencil.transform;
