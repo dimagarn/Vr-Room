62d81f2ec84d35ef86be04b722b96f3813b22f98
JunkerSoft <chebupelmen20@gmail.com>
2022-05-10 19:11:52 +0500

    Доработка синхронизации, требуется тестирование в очках

--- Assets/Online/PlayerNetwork.cs
+++ Assets/Online/PlayerNetwork.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using UnityEngine.XR;
 using Photon.Pun;
+using Valve.VR;
 using Valve.VR.InteractionSystem;
+using Hand = Valve.VR.InteractionSystem.Hand;
 
 public class PlayerNetwork : MonoBehaviour
 {
-    public GameObject dude;
+    public GameObject noneVRContent;
     public GameObject input;
-    public GameObject hand;
+    public GameObject noneVRHand;
+    public GameObject VRLeftHand;
+    public GameObject VRRightHand;
+    public GameObject VRCamera;
     private PhotonView photonView;
     
     // Start is called before the first frame update
@@ -17,9 +22,14 @@ public class PlayerNetwork : MonoBehaviour
         if (!photonView.IsMine)
         {
             input.SetActive(false);
-            hand.SetActive(false);
-            dude.GetComponent<Camera>().enabled = false;
-            dude.GetComponent<FallbackCameraController>().enabled = false;
+            noneVRHand.GetComponent<Hand>().enabled = false;
+            noneVRContent.GetComponent<Camera>().enabled = false;
+            noneVRContent.GetComponent<FallbackCameraController>().enabled = false;
+
+            VRCamera.GetComponent<Camera>().enabled = false;
+            VRCamera.GetComponent<SteamVR_Fade>().enabled = false;
+            VRLeftHand.GetComponent<Hand>().enabled = false;
+            VRRightHand.GetComponent<Hand>().enabled = false;
         }
     }
 }
